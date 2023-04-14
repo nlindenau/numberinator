48becfd3e888e37c6ab84d56f7835095c5322a21
Nicole Lindenau <nicole.lindenau@fraktio.fi>
2023-04-15 00:13:03 +0300

    fix: add main call

--- src/cloud-middleware/mw-service.py
+++ src/cloud-middleware/mw-service.py
@@ -1,16 +1,9 @@
-from dummy_values import numbers_sv, TTS_file
+from translator import get_number_translation_sv
+from tts import get_tts_file
 
 def get_user_number():
     user_number = int(input("What number would you like to be translated? "))
     return user_number
-
-def get_number_translation_sv(user_number):
-    number_translation = numbers_sv[user_number]
-    return number_translation
-
-def get_tts_file():
-    tts_file = TTS_file
-    return tts_file
 
 def main():
     user_number = get_user_number()
@@ -18,3 +11,5 @@ def main():
     tts_number_file = get_tts_file()
     print(f"Your TTS file for {user_number} in English: {tts_number_file}")
     print(f"{user_number} is {user_number_sv} in Swedish.")
+
+main()
