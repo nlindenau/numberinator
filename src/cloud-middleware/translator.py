1100da1a3593f43fec1b2f7d68621b9c0f9f77bb
Nicole Lindenau <nicole.lindenau@fraktio.fi>
2023-04-15 00:12:47 +0300

    refactor: create modules

---
+++ src/cloud-middleware/translator.py
@@ -0,0 +1,5 @@
+from dummy_values import numbers_sv
+
+def get_number_translation_sv(user_number):
+    number_translation = numbers_sv[user_number]
+    return number_translation
---
+++ src/cloud-middleware/tts.py
@@ -0,0 +1,5 @@
+from dummy_values import TTS_file
+
+def get_tts_file():
+    tts_file = TTS_file
+    return tts_file
