88bc1068d04df4bf6fafa983291da58b8953ecaf
Nicole Lindenau <nicole.lindenau@fraktio.fi>
2023-04-15 00:33:06 +0300

    feat: add dummy endpoints

--- src/cloud_middleware/__init__.py
+++ src/cloud_middleware/__init__.py
@@ -6,5 +6,13 @@ def create_app():
     @app.route('/')
     def hello():
         return 'Hello from cloud middleware service!'
+    
+    @app.route('/tts')
+    def produce_tts():
+        return 'I will return a TTS file!'
+
+    @app.route('/sv-translation')
+    def produce_sv_translation():
+        return 'I will return a Swedish translation of your number!'
 
     return app
