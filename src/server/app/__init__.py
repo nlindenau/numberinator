1c8bd30ab936a77d54bcb8db6a89bff3f0e39df8
Nicole Lindenau <nicole.lindenau@fraktio.fi>
2023-04-15 23:53:14 +0300

    feature: add dummy functions

--- src/server/app/__init__.py
+++ src/server/app/__init__.py
@@ -1,4 +1,15 @@
 def main():
     print("hello from the main hub")
     
+
+def get_translations():
+    print("I will get translations from cloud middleware")
+
+def get_tts():
+    print("I will get tts from cloud middleware")
+
+def get_number_recognition():
+    print("I will get a number recognition from digit recognition service")
+
+
 main()
