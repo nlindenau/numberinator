a8beb1c6f6823c7a001f6d2d0b2b27bb5c60ebfc
Nicole Lindenau <nicole.lindenau@fraktio.fi>
2023-04-15 11:09:53 +0300

    refactor: add function arguments

--- src/cloud_middleware/mw-service.py
+++ src/cloud_middleware/mw-service.py
@@ -1,15 +1,17 @@
 from cloud_middleware.translator import get_number_translation_sv
 from cloud_middleware.tts import get_tts_file
 
-def get_user_number():
-    user_number = int(input("What number would you like to be translated? "))
+def get_user_number(request):
+    user_number = request
     return user_number
 
-def run_cloud_middleware():
-    user_number = get_user_number()
+def run_cloud_middleware(request):
+    user_number = get_user_number(request)
     user_number_sv = get_number_translation_sv(user_number)
-    tts_number_file = get_tts_file()
+    tts_number_file = get_tts_file(user_number)
     print(f"Your TTS file for {user_number} in English: {tts_number_file}")
     print(f"{user_number} is {user_number_sv} in Swedish.")
+    
+    return user_number_sv, tts_number_file
 
 run_cloud_middleware()
--- src/cloud_middleware/tts.py
+++ src/cloud_middleware/tts.py
@@ -1,5 +1,5 @@
 from cloud_middleware.dummy_values import TTS_file
 
-def get_tts_file():
+def get_tts_file(user_number):
     tts_file = TTS_file
     return tts_file
