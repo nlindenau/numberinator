6117bfa51da9e83c289146a849fe7d13af1a1a02
Nicole Lindenau <nicole.lindenau@fraktio.fi>
2023-04-16 00:08:03 +0300

    feature: add .env support

--- src/server/app/__init__.py
+++ src/server/app/__init__.py
@@ -1,6 +1,17 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+SUBSCRIPTION_KEY = os.getenv('SUBSCRIPTION_KEY')
+CLOUD_MIDDLEWARE_HOST = os.getenv('CLOUD_MIDDLEWARE_HOST')
+CLOUD_MIDDLEWARE_PORT = os.getenv('CLOUD_MIDDLEWARE_PORT')
+DIGIT_RECOGNITION_HOST = os.getenv('DIGIT_RECOGNITION_HOST')
+DIGIT_RECOGNITION_PORT = os.getenv('DIGIT_RECOGNITION_PORT')
+
+
 def main():
-    print("hello from the main hub")
-    
+    print(f"I will connect to cloud middleware using {CLOUD_MIDDLEWARE_HOST}:{CLOUD_MIDDLEWARE_PORT}")
 
 def get_translations():
     print("I will get translations from cloud middleware")
