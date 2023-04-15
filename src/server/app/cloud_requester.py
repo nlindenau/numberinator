c3f97af39c64335e6f2586efa1157ec548693062
Nicole Lindenau <nicole.lindenau@fraktio.fi>
2023-04-16 00:25:23 +0300

    feat: add dummy cloud mw requester functions

--- src/server/app/cloud_requester.py
+++ src/server/app/cloud_requester.py
@@ -1,7 +1,23 @@
+def create_request(key_1, value_1, key_2, value_2):
+    """Creates a request body for cloud middleware service."""
+    request_body = {
+        key_1 : value_1,
+        key_2 : value_2,
+    }
 
+    return request_body
 
-def get_translations(number, subscription_key):
-    print("I will get translations from cloud middleware")
+def get_translations(user_number, subscription_key):
+    """Gets translations from the cloud middleware service."""
 
-def get_tts(written-number, subscription_key):
-    print("I will get tts from cloud middleware")
+    request = create_request("number", user_number, "subscription-key", subscription_key)
+
+    print(request)
+
+def get_tts(written_number, subscription_key):
+    """Gets a link to TTS file from the cloud middleware service."""
+
+    request = create_request("written-number", written_number, "subscription-key", subscription_key)
+
+    print(request)
+
