93880cf5610f662c92893dee959df10f719d5106
Nicole Lindenau <nicole.lindenau@fraktio.fi>
2023-04-16 12:41:07 +0300

    feat: add demo for getting translations

--- src/server/app/__init__.py
+++ src/server/app/__init__.py
@@ -1,6 +1,6 @@
 import os
 from dotenv import load_dotenv
-from app.cloud_requester import get_translations
+from cloud_requester import get_translations
 
 load_dotenv()
 
@@ -9,9 +9,18 @@ CLOUD_MIDDLEWARE_HOST = os.getenv('CLOUD_MIDDLEWARE_HOST')
 CLOUD_MIDDLEWARE_PORT = os.getenv('CLOUD_MIDDLEWARE_PORT')
 DIGIT_RECOGNITION_HOST = os.getenv('DIGIT_RECOGNITION_HOST')
 DIGIT_RECOGNITION_PORT = os.getenv('DIGIT_RECOGNITION_PORT')
+TRANSLATION_ENDPOINT = os.getenv('TRANSLATION_ENDPOINT')
 
+TRANSLATION_API_URL = CLOUD_MIDDLEWARE_HOST + ":" + CLOUD_MIDDLEWARE_PORT + TRANSLATION_ENDPOINT
 
 def main():
-    print(f"I will connect to cloud middleware using {CLOUD_MIDDLEWARE_HOST}:{CLOUD_MIDDLEWARE_PORT}")
+    my_number = 3 
+
+    print (f"Your number is {my_number}")
+
+    swedish_translation = get_translations(my_number, SUBSCRIPTION_KEY, TRANSLATION_API_URL)
+
+    print(f"In swedish, that's {swedish_translation}")
+
 
 main()
