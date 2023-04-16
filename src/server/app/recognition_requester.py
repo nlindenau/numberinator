bd8c1372a05e41b4a31605ae504894e6b8ab7bea
Nicole Lindenau <nicole.lindenau@fraktio.fi>
2023-04-16 20:10:42 +0300

    feat: add dummy module for calling digit recognition service

--- src/server/app/recognition_requester.py
+++ src/server/app/recognition_requester.py
@@ -1,2 +1,9 @@
-def get_number_recognition():
-    print("I will get a number recognition from digit recognition service")
+from request_processor import create_image_request, make_get_request, get_data_from_response
+
+def get_number_recognition(image, api_url):
+    """Returns prediction and confidence level of what number is written in the image via call to the digit recognition service.
+    """
+    request_body = create_image_request("image-base64", image)
+    response = make_get_request(api_url, request_body)
+
+    return response
