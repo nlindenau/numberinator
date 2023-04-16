f61009a2be4f60072529d0ae5f49183118959b43
Nicole Lindenau <nicole.lindenau@fraktio.fi>
2023-04-16 12:40:25 +0300

    feat: add request handler for cloud mw

--- src/server/app/cloud_requester.py
+++ src/server/app/cloud_requester.py
@@ -1,18 +1,26 @@
+import requests
+
 def create_request(key_1, value_1, key_2, value_2):
     """Creates a request body for cloud middleware service."""
     request_body = {
-        key_1 : value_1,
-        key_2 : value_2,
+        key_1: value_1,
+        key_2: value_2,
     }
 
     return request_body
 
-def get_translations(user_number, subscription_key):
+def get_translations(user_number, subscription_key, api_url):
     """Gets translations from the cloud middleware service."""
 
     request = create_request("number", user_number, "subscription-key", subscription_key)
 
-    print(request)
+    response = requests.get(api_url, json=request)
+    
+    response_contents = response.json()
+
+    translation = response_contents["swedish_translation"]
+
+    return translation
 
 def get_tts(written_number, subscription_key):
     """Gets a link to TTS file from the cloud middleware service."""
